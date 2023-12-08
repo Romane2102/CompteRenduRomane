0d6a8e7a3ab6e2b2280890050ef0b391528435cd
romane <romane.trombetta@gmail.com>
2023-12-08 10:16:18 +0100

    Ajout de la classe LectureMain pour créer et écrire dans un .xml

--- src/main/java/com/soprasteria/exo2/LectureMain.java
+++ src/main/java/com/soprasteria/exo2/LectureMain.java
@@ -1,9 +1,12 @@
 package com.soprasteria.exo2;
 import java.time.LocalDate;
 import java.util.Scanner;
-
+import java.io.FileWriter;
+import java.io.IOException;
 import org.jdom2.Document;
 import org.jdom2.Element;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
 
 import com.soprasteria.exo2.CompteBancaire;
 
@@ -18,8 +21,24 @@ public class LectureMain {
 		
 		//Construire des objets de type Animal
 		Scanner clavier = new Scanner(System.in);	
-		CompteBancaire c;
-			c = getInfos(clavier);
+		CompteBancaire cpt1 = getInfos(clavier);
+		CompteBancaire cpt2 = getInfos(clavier);
+		CompteBancaire cpt3 = getInfos(clavier);
+		
+		//Attacher ces objets à l'élément racine
+		doc.getRootElement().addContent(createCompteBancaireXMLElement(cpt1));
+		doc.getRootElement().addContent(createCompteBancaireXMLElement(cpt2));
+		doc.getRootElement().addContent(createCompteBancaireXMLElement(cpt3));
+		
+		//Écriture dans un fichier .xml
+		XMLOutputter xmlOutput = new XMLOutputter();
+		xmlOutput.setFormat(Format.getPrettyFormat());
+		try {
+			xmlOutput.output(doc, new FileWriter("ComptesBancaires.xml"));
+		} catch (IOException io) {
+			System.out.println(io.getMessage());
+		}
+		System.out.println("Fichier sauvegardé !");
 			
 	}
 
@@ -39,6 +58,18 @@ public class LectureMain {
 		LocalDate datecreation = LocalDate.of(annee, mois, jour);
 		System.out.println("Entrez le type de compte :");
 		String typecpte = clavier.next();
+		return new CompteBancaire(numcpte, nom, solde, datecreation, typecpte);
+	}
+		
+		private static Element createCompteBancaireXMLElement(CompteBancaire cptebancaire) {
+	        Element compteBancaireElement = new Element("CompteBancaire");
+	        compteBancaireElement.addContent(new Element("NumeroCompte").setText(""+cptebancaire.getNumCompte()));
+	        compteBancaireElement.addContent(new Element("NomProprietaire").setText(cptebancaire.getNomProprietaire()));
+	        compteBancaireElement.addContent(new Element("Solde").setText(""+cptebancaire.getSolde()));
+	        compteBancaireElement.addContent(new Element("DateCreation").setText(""+cptebancaire.getDateCreation()));
+	        compteBancaireElement.addContent(new Element("TypeCompte").setText(cptebancaire.getTypeCompte()));
+	        return compteBancaireElement;
+		
 		
 	}
 
